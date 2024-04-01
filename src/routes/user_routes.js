5a0c71f5b869d2fef2c858839f2a6fb984e9eebf
JulianaChM <julianchamorromarin@gmail.com>
2024-03-31 20:36:57 -0500

    user_routes.js

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -2,11 +2,8 @@ const userController = require("../controllers/user_controller");
 const express = require("express");
 const router = express.Router();
 
-// http:localhost:3000/api/v1/users/new-user
-router.post("/new-user", userController.createUser);
-router.get("/", userController.listUsers);
-router.get("/:id", userController.getUser); // : indica que lo que sigue es un parámetro
-router.patch("/edit/:id", userController.editUser);
-router.delete("/delete/:id", userController.deleteUser);
+// http:localhost:3000/api/v1/users/signup
+router.post("/signup", userController.signUpUser);
+router.post("/login", userController.loginUser);
 
 module.exports = router;
